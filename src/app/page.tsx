5858e7a0da0d49c5bd44391ca123b44b70f0799a
artomily <vararakya@gmail.com>
2025-11-30 20:43:13 +0700

    feat: Add wallet connection, balance display, and escrow management features
    - Implemented WalletConnection component for wallet connectivity.
    - Created Balance component to display native and mock coin balances.
    - Added ListMyEscrow component to list user's escrows with details.
    - Developed Escrow component for creating, accepting, and canceling escrows.
    - Introduced Faucet component for claiming mock coins.
    - Integrated NFTGallery component to display user's NFTs.
    - Established Providers component for managing Sui client and query client.
    - Updated main page to include new components for enhanced user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
+import Balance from "@/components/Balance";
+import Escrow from "@/components/transaction/Escrow";
+import Faucet from "@/components/transaction/Faucet";
+import ListMyEscrow from "@/components/ListMyEscrow";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { WalletConnection } from "@/components/WalletConnection";
+import { ConnectButton } from "@mysten/dapp-kit";
 
 export default function page() {
   return (
@@ -9,6 +15,13 @@ export default function page() {
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
         <span>Start Here!</span>
+        {/* <ConnectButton /> */}
+        <WalletConnection />
+        <Balance />
+        <Faucet />
+        <Escrow />
+        <ListMyEscrow />
+
       </CardContent>
     </Card>
   )
